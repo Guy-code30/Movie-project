f2ca439e8ba942d2b2b8e7b5846cbede5c8ab888
Sam <samellis1403@gmail.com>
2025-01-11 13:59:58 -0500

    update

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
-async function main() {
-  const response = await fetch("https://www.omdbapi.com/?apikey=490768e9&s=Fast");
-  const data = await response.json();
-  console.log(data);
+async function movies() {
+  const response = await fetch(
+    "https://www.omdbapi.com/?apikey=490768e9&s=Fast"
+  );
+  const movieData = await response.json();
+  const movieListEl = document.querySelector(".movielist");
+  movieListEl.innerHTML = movieData.map((movie) => movieHTML(movie)).join("");
 }
+
+movie();
